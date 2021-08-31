9977f06f709e582f1ad12bd90d692f5a87f00778
Fiorella <fofi-100@hotmail.com>
2021-08-31 11:24:02 -0500

    Âsemaforo

---
+++ main.js
@@ -0,0 +1,15 @@
+//Ejercicio de color del semáforo.
+let semaforo = "morado"
+
+if (semaforo == "verde") {
+    console.log ("Puedes avanzar")
+} else if (semaforo == "rojo") {
+console.log ("No puedes avanzar")
+} else if (semaforo == "amarillo") {
+console.log ("Espera")
+} else {
+console.log ("Color no válido")
+} 
+
+
+
